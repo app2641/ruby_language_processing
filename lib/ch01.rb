b0c7783ee829810ea3d78c0a5afadb24f3e71db6
app2641 <app2641@gmail.com>
2021-01-19 00:18:27 +0900

    Element symbol

--- lib/ch01.rb
+++ lib/ch01.rb
@@ -21,6 +21,17 @@ class Ch01
   def pai
     str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
     list = str.gsub(/,|\./, '').split(' ')
-    list.map(&:length)
+    list.map(&:size)
+  end
+
+  def element_symbol
+    single = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+    str = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
+    list = str.gsub(/\./, '').split(' ')
+
+    list.map.with_index(1) do |l, i|
+      el = single.include?(i) ? l.slice(0) : l.slice(0..1)
+      [el, i]
+    end.to_h.transform_keys(&:to_sym)
   end
 end
--- spec/lib/ch01_spec.rb
+++ spec/lib/ch01_spec.rb
@@ -15,11 +15,40 @@ RSpec.describe Ch01, type: :model do
     it { expect(klass.patatokucassy).to eq 'パタトクカシーー' }
   end
 
-  describe 'pai' do
+  describe '#pai' do
     let(:expected) do
       [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
     end
 
     it { expect(klass.pai).to eq expected }
+  end
+
+  describe '#element_symbol' do
+    let(:expected) do
+      {
+        'H': 1,
+        'He': 2,
+        'Li': 3,
+        'Be': 4,
+        'B': 5,
+        'C': 6,
+        'N': 7,
+        'O': 8,
+        'F': 9,
+        'Ne':10,
+        'Na': 11,
+        'Mi': 12,
+        'Al': 13,
+        'Si': 14,
+        'P': 15,
+        'S': 16,
+        'Cl': 17,
+        'Ar': 18,
+        'K': 19,
+        'Ca': 20
+      }
+    end
+
+    it { expect(klass.element_symbol).to eq expected }
   end
 end
