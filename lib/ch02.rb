c1b0b2b8e634a7d691592c5762e2474a423dcab6
app2641 <app2641@gmail.com>
2021-01-25 20:32:36 +0900

    Sed

--- lib/ch02.rb
+++ lib/ch02.rb
@@ -4,4 +4,8 @@ class Ch02
   def line_count(file_path)
     `wc -l #{file_path}`
   end
+
+  def sed(file_path)
+    `sed -e 's/\t/\s/g' #{file_path}`
+  end
 end
--- spec/lib/ch02_spec.rb
+++ spec/lib/ch02_spec.rb
@@ -2,10 +2,13 @@
 
 RSpec.describe Ch02, type: :model do
   let(:klass) { described_class.new }
+  let(:txt_path) { File.expand_path('./data/popular-names.txt') }
 
   describe '#line_count' do
-    let(:txt_path) { File.expand_path('./data/popular-names.txt') }
-
     it { expect(klass.line_count(txt_path)).to match /2780/ }
+  end
+
+  describe '#sed' do
+    it { expect(klass.sed(txt_path)).not_to match /\t/ }
   end
 end
